db7f9d74a2f6681ad6a11f0eb05cfcae613cb9dd
The Ghost <tghostlycat@gmail.com>
2021-09-27 06:48:22 +0300

    Apply sane styling to vue components

--- src/js/components/SplideSlide.vue
+++ src/js/components/SplideSlide.vue
@@ -1,11 +1,11 @@
 <template>
-  <li class="splide__slide">
-    <slot></slot>
-  </li>
+	<li class="splide__slide">
+		<slot></slot>
+	</li>
 </template>
 
 <script>
-	export default {
-		name: 'SplideSlide',
-	};
+export default {
+	name: "SplideSlide",
+};
 </script>
